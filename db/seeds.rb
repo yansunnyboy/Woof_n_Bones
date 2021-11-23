106339b5d10695461db82a604b05322b97511a33
Sarah Alexander <sjalex78@gmail.com>
2021-11-23 21:23:14 +1100

    seeded the db data now in.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!({ email: 'hi@gmail.com', encrypted_password: 'helloworld', first_name: 'Luke', last_name: 'Skywalker',
+              contact_number: '0412439529', location: '123 canberra court, northcutt', password: 'password' })
 
-Centre.create([{ name: 'Doggy Home', location: '123 high street', phone_number: '95576165', capacity: 29 },
-               { name: 'Dog House', location: '1023 collins st, melbourne', phone_number: '95676165', capacity: 80 },
-               { name: 'Snoop Dogg', location: '12 jimmy parade, mulgrave', phone_number: '956146165', capacity: 10 }])
+Centre.create!([{ name: 'Doggy Home', location: '123 high street', phone_number: '95576165', capacity: 29, user_id: 1  },
+               { name: 'Dog House', location: '1023 collins st, melbourne', phone_number: '95676165', capacity: 80, user_id: 2 },
+               { name: 'Snoop Dogg', location: '12 jimmy parade, mulgrave', phone_number: '956146165', capacity: 10, user_id: 1  }])
 
-Dog.create([{ name: 'pupper', dog_size: 's', diet: 'meat', breed: 'shiba', desexed: true, vaccination_status: true,
+Dog.create!([{ name: 'pupper', dog_size: 's', diet: 'meat', breed: 'shiba', desexed: true, vaccination_status: true,
               user_id: 1 },
             { name: 'jim', dog_size: 'm', diet: 'tuna', breed: 'sausage', desexed: true, vaccination_status: true,
               user_id: 1 },
             { name: 'cookie', dog_size: 'l', diet: 'vegan', breed: 'husky', desexed: true, vaccination_status: true,
               user_id: 1 }])
-
-User.create({ email: 'hi@gmail.com', encrypted_password: 'helloworld', first_name: 'Luke', last_name: 'Skywalker',
-              contact_number: '0412439529', location: '123 canberra court, northcutt' })
