07acfd572c4bf29f63ea0ab14a037173141ea85d
Yan Sun <ysun971@gmail.com>
2021-11-24 12:20:14 +1100

    Update on home page for search bar with edits to css templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'centres#index'
+  root to: 'pages#home'
   resources :centres do
     resources :bookings, only: %i[new create]
   end
