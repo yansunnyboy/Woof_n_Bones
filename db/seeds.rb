3b4f2191f3c1c2e2029978ca1004fd4271557e8b
Sarah Alexander <sjalex78@gmail.com>
2021-11-26 14:23:00 +1100

    seed file changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,29 @@
 
 require 'faker'
 
+User.create([{ email: 'hi@gmail.com', encrypted_password: 'helloworld', first_name: 'Luke', last_name: 'Skywalker',
+               contact_number: '0412439529', location: '123 canberra court, northcutt' },
+             { email: 'joe@gmail.com', encrypted_password: 'helloworld', first_name: 'Joe', last_name: 'Mama',
+               contact_number: '0412439629', location: '123 adelaide court, appleseed' },
+             { email: 'hilly@gmail.com', encrypted_password: 'helloworld', first_name: 'Hillary', last_name: 'Clinton',
+               contact_number: '0412439529', location: '123 darwin lane, daylesford' }])
+
+Centre.create([{ name: 'Doggy Home', location: '123 high street', phone_number: '95576165', capacity: 29, user_id: 1 },
+               { name: 'Dog House', location: '1023 collins st, melbourne', phone_number: '95676165', capacity: 80,
+                 user_id: 3 },
+               { name: 'Snoop Dogg', location: '12 jimmy parade, mulgrave', phone_number: '956146165', capacity: 10,
+                 user_id: 3 }])
+
+Dog.create([{ name: 'pupper', dog_size: 's', diet: 'meat', breed: 'shiba', desexed: true, vaccination_status: true,
+              user_id: 1 },
+            { name: 'jim', dog_size: 'm', diet: 'tuna', breed: 'sausage', desexed: true, vaccination_status: true,
+              user_id: 2 },
+            { name: 'cookie', dog_size: 'l', diet: 'vegan', breed: 'husky', desexed: true, vaccination_status: true,
+              user_id: 2 }])
+
+
+
+
 50.times do
   User.create!(
     email: Faker::Internet.email,
